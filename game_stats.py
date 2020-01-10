a6fac47dd1a8f884c3aef97f1a6d54b4cce5f29d
SuperDuperMeee <michael.g.r.bennett@outlook.com>
2020-01-10 17:37:49 +0000

    Chapter 3 - Adding dynamic settings and scores

--- game_stats.py
+++ game_stats.py
@@ -4,9 +4,10 @@ class GameStats:
     def __init__(self, ai_game):
         """Initialise statistics"""
         self.settings = ai_game.settings
-        self.reset_stats
-        # Start Alien Invasion in an active state.
-        self.game_active = True
+        self.reset_stats()
+        # Start Alien Invasion in an inactive state.
+        self.game_active = False
+
 
     def reset_stats(self):
         """Initialise statistics that can change during the game."""
